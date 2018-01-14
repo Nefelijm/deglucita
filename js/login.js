fafd9f3cc829ce67463d5115b09195fa9ced21ef
Nefelijm <sistemasnik20@gmail.com>
2018-01-14 10:30:04 -0500

    Funcionalidad finalizada para login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,62 @@
 $(document).ready(function () {
+
+ //Funciones para elegir las resgistrarse o iniciar sesion
+    $('#change1').click(function () {
+      $('#btnSign').removeClass("hide");
+      $('#btnLogin').addClass("hide");
+      
+    });
+  $('#change2').click(function () {
+    $('#btnSign').addClass("hide");
+    $('#btnLogin').removeClass("hide");
+
+  });
+  // Creando variables booleanas que nos ayudaran 
+  var verifyemail = false;
+  var verifyPassword = false;
+
+  function activeButton() {
+    if (verifyemail && verifyPassword) {
+      $('#btnLogin').attr('disabled', false);
+      $('#btnSign').attr('disabled', false);
+    }
+  }
+
+  function desactiveButton() {
+    if (verifyemail && verifyPassword) {
+      $('#btnLogin').attr('disabled', true);
+      $('#btnSign').attr('disabled', true);
+    }
+  }      
+
+  // Funcion para validar el input de email
+  $('#textEmail').on('input', function () {
+    var parameterEmail = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/); // Expresiones regulares que nos ayudara a validar el email
+    if (parameterEmail.test($(this).val()) === false || $(this).val() === '') {
+      $('#msj1').text('Ingrese su correo correcto');
+      desactiveButton();
+      return false;
+    } else {
+      $('#msj1').text('');
+      verifyemail = true;
+      activeButton();
+    }
+  });
+
+  // validando el Input de password
+  $('#textPassword').on('input', function (event) {
+    if ($(this).val().length < 6 || $(this).val() === '') {
+      $('#msj2').text('Ingrese una contraseña mayor a 6 digitos');
+      desactiveButton();
+      return false;
+    } else {
+      $('#msj2').text('');
+      verifyPassword = true;
+      activeButton();
+    }
+  });
+
+
   // Initialize Firebase
   var config = {
     apiKey: 'AIzaSyAlbcybJZDOmr_rLwWDzAnarW7U23C8ZyI',
@@ -28,8 +86,7 @@ $(document).ready(function () {
     var auth = firebase.auth();
     // Hacemos el metodo de identficacion 
     var promise = auth.signInWithEmailAndPassword(emailLogin, passwordLogin);
-    promise.catch(e => console.log(e.message));
-    
+    promise.catch(e => console.log(e.message));  
 
   });
 
